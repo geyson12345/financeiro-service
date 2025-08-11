cdee034fdfc61208876a60140d056fdb7854baee
geyson12345 <geysongomes6@gmail.com>
2025-08-11 12:28:32 -0300

    Sistema Financeiro

---
+++ src/main/java/br/com/finchsolucoes/financeiro/service/FinanceiroServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.finchsolucoes.financeiro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroServiceApplication.class, args);
+	}
+
+}
